a26c7ad000da8a99815aedb341e65f6e2544e43e
Abukhalil95 <yahya_abukhaleel@hotmail.com>
2020-10-05 00:42:32 +0300

    stash

--- src/timetable/start-study.js
+++ src/timetable/start-study.js
@@ -12,12 +12,7 @@ const history = require('./lib/collections/history-collection');
 const weekly = require('./lib/collections/weekly-collection');
 
 const mongoose = require('mongoose');
-<<<<<<< HEAD
 const MONGOOSE_URL = process.env.MONGOOSE_URL ;
-=======
-// const MONGOOSE_URL = 'mongodb://localhost:27017/jadwalla';
-const MONGOOSE_URL = 'mongodb+srv://admin:admin@cluster0.m6biy.mongodb.net/jadwal?retryWrites=true&w=majority';
->>>>>>> master
 mongoose.connect(MONGOOSE_URL, {
   useNewUrlParser : true,
   useUnifiedTopology : true,
@@ -48,7 +43,7 @@ const table = {
 };
 
 let date = 1;
-let validTemplates;
+let starterTemplates;
 let pastTemplates;
 
 async function getInput(choice, template) {
@@ -100,53 +95,63 @@ async function getInput(choice, template) {
   }, 3000);
 }
 
-async function getTemplate(validTemplates, pastTemplates) {
+async function getTemplate(starterTemplates, pastTemplates) {
   let subjectsToChoose = [];
-  let inputNum = -1;
-  if (!pastTemplates) {
-    waitForDebugger;
-  }
-  console.log('validTemplates \n', validTemplates);
-
+  let choice = -1;
   let validTemplateNames = [];
-  for (var keys in validTemplates) {
-    validTemplateNames.push(validTemplates[keys].name);
+  let chosenTemplate = {};
+
+  for (let keys in starterTemplates) {
+    validTemplateNames.push(starterTemplates[keys].name);
   }
-  console.log('validTemplateNames \n', validTemplateNames);
+  for (let keys in pastTemplates) {
+    validTemplateNames.push(pastTemplates[keys].name);
+  }
+  console.log('Valid Template Names \n', validTemplateNames);
+
   do {
     const input = await inquirer.prompt([
-      { name: 'template', message: 'Choose a template from the list with number \n' },
+      { name: 'template', message: 'Choose a template from the list with a number \n' },
     ]);
-    inputNum = input.template.split(' ')[0] - 1;
-  } while (!(!isNaN(inputNum) && inputNum >= 0 && inputNum < validTemplateNames.length));
-  console.log('chosen: ', inputNum);
+    choice = input.template.split(' ')[0] - 1;    // gets first input and checks if its within limit
+  } while (!(!isNaN(choice) && choice >= 0 && choice < validTemplateNames.length));
 
-  if (validTemplates[inputNum].subjects.length <= 0) {
-    throw new Error('Invalid Template');
+  console.log('Chosen: ', choice);            // gets the valid choice and gets the template
+  if (choice >= starterTemplates.length) {
+    console.log('Choosing from past template at ' + choice - starterTemplates.length);
+    chosenTemplate = pastTemplates[choice - starterTemplates.length];
+  } else {
+    console.log('Choosing from starter template at ' + choice);
+    chosenTemplate = starterTemplates[choice];
   }
 
-  for (let k = 0; k < validTemplates[inputNum].subjects.length; k++) {      //fills in the chapters of each unit and lesson as a choice table
-    for (let i = 0; i < validTemplates[inputNum].subjects[k].units.length; i++) {
-      for (let j = 0; j < validTemplates[inputNum].subjects[k].units[i].chapters.length; j++) {
-        subjectsToChoose.push([
-          validTemplates[inputNum].subjects[k].name,
-          'Chapter ' + validTemplates[inputNum].subjects[k].units[i].number, 
-          'Lesson ' + validTemplates[inputNum].subjects[k].units[i].chapters[j
-          ]]);
-      }
-    }
+  if (chosenTemplate.subjects.length <= 0) {
+    console.log('Invalid template ' + choice);
+    getTemplate(starterTemplates, pastTemplates);
   }
 
-  console.log('adding new template attached to the user..');
-  validTemplates[inputNum].name += ' user';
-  console.log(`saving.. >>>>>>>>> ${validTemplates[inputNum].name}`);
+  // for (let k = 0; k < starterTemplates[choice].subjects.length; k++) {      //fills in the chapters of each unit and lesson as a choice table
+  //   for (let i = 0; i < starterTemplates[choice].subjects[k].units.length; i++) {
+  //     for (let j = 0; j < starterTemplates[choice].subjects[k].units[i].chapters.length; j++) {
+  //       subjectsToChoose.push([
+  //         starterTemplates[choice].subjects[k].name,
+  //         'Chapter ' + starterTemplates[choice].subjects[k].units[i].number, 
+  //         'Lesson ' + starterTemplates[choice].subjects[k].units[i].chapters[j
+  //         ]]);
+  //     }
+  //   }
+  // }
 
-  let historyTemplate = validTemplates[inputNum].toObject();      // cant create new mongoose entry before converting it to object
-  await history.create(historyTemplate);
-  console.log(`saved.. >>>>>>>>> \n`);
-  console.log(util.inspect(historyTemplate, false, null, true /* enable colors */));
+  // console.log('adding new template attached to the user..');
+  // starterTemplates[choice].name += ' user';
+  // console.log(`saving.. >>>>>>>>> ${starterTemplates[choice].name}`);
 
-  getInput(subjectsToChoose, inputNum);
+  // let historyTemplate = starterTemplates[choice].toObject();      // cant create new mongoose entry before 
+  // await history.create(historyTemplate);                          // converting it back from mongoose document to object
+  // console.log(`saved.. >>>>>>>>> \n`);
+  // console.log(util.inspect(historyTemplate, false, null, true /* enable colors */));
+
+  // getInput(subjectsToChoose, choice);
 }
 
 
@@ -154,11 +159,11 @@ async function startApp() {
   console.clear();
   console.log('reading..');
 
-  pastTemplates = await history.read();
-  validTemplates = await template.read();
   // await template.clear();
   // await fillEmptyDB();
-  await getTemplate(validTemplates, pastTemplates);
+  pastTemplates = await history.read();
+  starterTemplates = await template.read();
+  await getTemplate(starterTemplates, pastTemplates);
 
   // mongoose.disconnect();
 }
@@ -168,14 +173,14 @@ function getHistory() {
 }
 
 async function updateHistory(topic, template) {
-  for (let i = 0; i < validTemplates[template].subjects.length; i++) {
-    if (validTemplates[template].subjects[i].name == topic[0]) {
-      validTemplates[template].subjects[i].units[topic[1].split(' ')[1] - 1].completed++; //change completed
-      console.log('Update completion! ', validTemplates[template].subjects[i].units[topic[1].split(' ')[1] - 1]);
+  for (let i = 0; i < starterTemplates[template].subjects.length; i++) {
+    if (starterTemplates[template].subjects[i].name == topic[0]) {
+      starterTemplates[template].subjects[i].units[topic[1].split(' ')[1] - 1].completed++; //change completed
+      console.log('Update completion! ', starterTemplates[template].subjects[i].units[topic[1].split(' ')[1] - 1]);
     }
   }
-  console.log(`updating.. >>>>>>>>> ${validTemplates[template].name} with id ${validTemplates[template]._id}`);
-  await history.update(validTemplates[template]._id, validTemplates[template]);
+  console.log(`updating.. >>>>>>>>> ${starterTemplates[template].name} with id ${starterTemplates[template]._id}`);
+  await history.update(starterTemplates[template]._id, starterTemplates[template]);
 }
 
 
