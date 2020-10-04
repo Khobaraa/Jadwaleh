edf389d8a7fc95d8c0b1a56796c66f760f0bf073
Abukhalil95 <yahya_abukhaleel@hotmail.com>
2020-10-04 12:51:57 +0300

    finished taskChooser 90%

---
+++ src/timetable/lib/collections/weekly-collection.js
@@ -0,0 +1,35 @@
+'use strict';
+// require the schema and move all notes crud operations
+const mongoose = require('mongoose');
+const Model = require('../mongo.js');
+
+const schema = mongoose.model('table', {
+  week: [
+    {
+      number: {type: String, require: true},
+      totalHoursWeek: {type: Number, default: 0},
+      day: [
+        {
+          number: {type: Number},
+          topics: [
+            {
+              name: {type: String, default: 0},
+              totalHours: {type: Number, default: 0}, // for future 
+              completed: {type: Number, default: 0}, // 0-1
+            },
+          ],
+          totalHoursDay: {type: Number, default: 0},
+        },
+      ],
+    },
+  ],
+});
+
+class Table extends Model{
+  constructor() {
+    super(schema);
+  }
+
+}
+
+module.exports = new Table;
