ae4bc1409e8321d14a3de37904511d75dcbab15a
Abukhalil95 <yahya_abukhaleel@hotmail.com>
2020-11-03 13:38:03 +0200

    hotfix

--- src/modules/statistics.js
+++ src/modules/statistics.js
@@ -1,37 +1,37 @@
-'use strict';
+// 'use strict';
 
-const history = require('./history-collection');
+// const history = require('./history-collection');
 
-module.exports = async function (userId) {
-  let data = await history.read(userId);
-  let statArr = [];
-  try {
-    data = data[0].toObject();
+// module.exports = async function (userId) {
+//   let data = await history.read(userId);
+//   let statArr = [];
+//   try {
+//     data = data[0].toObject();
 
-    data.courses.forEach(course => {
-      let result = statistics(course);
-      statArr.push(result);
+//     data.courses.forEach(course => {
+//       let result = statistics(course);
+//       statArr.push(result);
       
-    });
-    return statArr;
-  } catch(e) {
-    throw new Error('You have not started studying yet!');
-  }
-};
+//     });
+//     return statArr;
+//   } catch(e) {
+//     throw new Error('You have not started studying yet!');
+//   }
+// };
 
-function statistics (course) {
-  let result ={}, total = 0 , hours = 0;
-  result['name'] = course.name;
-  result['hours'] = course.expectedHours;
-  course.chapters.forEach(chapter => {
+// function statistics (course) {
+//   let result ={}, total = 0 , hours = 0;
+//   result['name'] = course.name;
+//   result['hours'] = course.expectedHours;
+//   course.chapters.forEach(chapter => {
       
-    if(chapter.state === 'completed'){
-      total++;
-      hours += Math.floor(chapter.duration*50*Math.random()); // numbers randomizer
-    }
-    result['spentHours'] = hours;
-    result['progress'] = Math.floor(total/ course.noOfChapters * 100);
+//     if(chapter.state === 'completed'){
+//       total++;
+//       hours += Math.floor(chapter.duration*50*Math.random()); // numbers randomizer
+//     }
+//     result['spentHours'] = hours;
+//     result['progress'] = Math.floor(total/ course.noOfChapters * 100);
 
-  });
-  return result;
-}
+//   });
+//   return result;
+// }
